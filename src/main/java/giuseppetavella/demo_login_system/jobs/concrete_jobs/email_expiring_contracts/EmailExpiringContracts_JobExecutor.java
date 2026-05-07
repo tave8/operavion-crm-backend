805842a5573671cd8527ccd787dce3be9cded466
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-05-07 08:39:25 +0200

    notification system: add notifications from background job

--- src/main/java/giuseppetavella/demo_login_system/jobs/concrete_jobs/email_expiring_contracts/EmailExpiringContracts_JobExecutor.java
+++ src/main/java/giuseppetavella/demo_login_system/jobs/concrete_jobs/email_expiring_contracts/EmailExpiringContracts_JobExecutor.java
@@ -1,12 +1,15 @@
 package giuseppetavella.demo_login_system.jobs.concrete_jobs.email_expiring_contracts;
 
+import giuseppetavella.demo_login_system.entities.Notification;
 import giuseppetavella.demo_login_system.entities.User;
+import giuseppetavella.demo_login_system.enums.NotificationType;
 import giuseppetavella.demo_login_system.jobs.JobExecutionItem;
 import giuseppetavella.demo_login_system.jobs.JobExecutionMetadata;
 import giuseppetavella.demo_login_system.jobs.JobExecutionService;
 import giuseppetavella.demo_login_system.jobs.JobExecutor;
 import giuseppetavella.demo_login_system.jobs.enums.JobName;
 import giuseppetavella.demo_login_system.services.AppEmailService;
+import giuseppetavella.demo_login_system.services.NotificationsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -23,7 +26,7 @@ public class EmailExpiringContracts_JobExecutor extends JobExecutor<User> {
     private AppEmailService appEmailService;
     
     @Autowired
-    private JobExecutionService jobExecutionService;
+    private NotificationsService notificationsService;
     
     
     public EmailExpiringContracts_JobExecutor() {
@@ -41,17 +44,33 @@ public class EmailExpiringContracts_JobExecutor extends JobExecutor<User> {
         // send email, do business-specific logic
         User user = (User) itemToProcess.getItem();
         
-        jobExecutionMetadata.getExtra().put("firstname", user.getFirstname());
-        
-        // metadata.getProcessedItemIds().removeIf((x) -> x.equals("id1"));
-        
-        
-        try {
-            Thread.sleep(2000);
+        // add notification in DB
 
-        } catch (InterruptedException e) {
-            // throw new RuntimeException(e);
-        }
+        Notification newNotification = new Notification(
+                user,
+                NotificationType.EXPIRING_EMPLOYEE_CONTRACT,
+                "this employee's contract is expiring",
+                "<added by background job>"
+        );
+        
+        this.notificationsService.save(
+                newNotification
+        );
+        
+        // send an email to the employee
+        // this.appEmailService.sendEmail(
+        //         user.getEmail(),
+        //         "expiring employee",
+        //         "The employee " + user.getFirstname() + " is expiring. Here's their profile picture: " + user.getAvatarUrl()
+        // );
+        
+        
+        // try {
+        //     // Thread.sleep(2000);
+        //
+        // } catch (InterruptedException e) {
+        //     // throw new RuntimeException(e);
+        // }
         
         
         
