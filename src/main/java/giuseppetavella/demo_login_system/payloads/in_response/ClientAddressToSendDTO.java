90ebcd959d33c9a23904f7f1f0d45d7230cb0f5d
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-05-11 14:08:22 +0200

    get client addresses & more fields

--- src/main/java/giuseppetavella/demo_login_system/payloads/in_response/ClientAddressToSendDTO.java
+++ src/main/java/giuseppetavella/demo_login_system/payloads/in_response/ClientAddressToSendDTO.java
@@ -8,15 +8,39 @@ public class ClientAddressToSendDTO {
     private final UUID clientId;
     private final UUID addressId;
     private final String addressName;
+    private final String clientName;
+    private final String addressDisplayName;
+    private final double addressLat;
+    private final double addressLon;
 
     public ClientAddressToSendDTO(ClientAddress clientAddress) {
         this.addressId = clientAddress.getAddress().getId();
         this.clientId = clientAddress.getClient().getId();
         this.addressName = clientAddress.getAddressName();
+        this.clientName = clientAddress.getClient().getLegalName();
+        this.addressDisplayName = clientAddress.getAddress().getDisplayName();
+        this.addressLat = clientAddress.getAddress().getLat();
+        this.addressLon = clientAddress.getAddress().getLon();
     }
 
     public UUID getAddressId() {
         return addressId;
+    }
+
+    public String getAddressDisplayName() {
+        return addressDisplayName;
+    }
+
+    public double getAddressLat() {
+        return addressLat;
+    }
+
+    public String getClientName() {
+        return clientName;
+    }
+
+    public double getAddressLon() {
+        return addressLon;
     }
 
     public String getAddressName() {
