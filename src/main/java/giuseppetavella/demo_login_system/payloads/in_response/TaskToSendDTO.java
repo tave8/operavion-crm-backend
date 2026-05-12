a72d98dd1f0dd94569207d99df1ebe02be365b9c
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-05-12 04:54:38 +0200

    add taskId when send task

--- src/main/java/giuseppetavella/demo_login_system/payloads/in_response/TaskToSendDTO.java
+++ src/main/java/giuseppetavella/demo_login_system/payloads/in_response/TaskToSendDTO.java
@@ -2,12 +2,20 @@ package giuseppetavella.demo_login_system.payloads.in_response;
 
 import giuseppetavella.demo_login_system.entities.Task;
 
+import java.util.UUID;
+
 public class TaskToSendDTO {
     
+    private final UUID taskId;
     private final String name;
     
     public TaskToSendDTO(Task task) {
+        this.taskId = task.getId();
         this.name = task.getName();
+    }
+
+    public UUID getTaskId() {
+        return taskId;
     }
 
     public String getName() {
