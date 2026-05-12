8eaab73a2a2f92643e50041661597eb38e535909
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-05-12 02:59:44 +0200

    create entity Checklist

---
+++ src/main/java/giuseppetavella/demo_login_system/entities/Checklist.java
@@ -0,0 +1,52 @@
+package giuseppetavella.demo_login_system.entities;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "checklists")
+public class Checklist {
+    
+    @Id
+    @GeneratedValue
+    private UUID id;
+    
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Company company;
+
+    @Column(nullable = false)
+    private String name;
+    
+    protected Checklist() {}
+    
+    public Checklist(Company company, String name) 
+    {
+        
+        this.company = company;
+        this.name = name;
+        
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return "Checklist{" +
+                "company=" + company +
+                ", id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
