9e8dc2e4ec0f13cecad4e3ed05e11fc3e5fc83ee
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-05-24 16:43:37 +0200

    fix: database schema "stripe subscription status"

--- src/main/resources/db/migration/V2_1__fix_stripe_subscription_status.sql
+++ src/main/resources/db/migration/V2_1__fix_stripe_subscription_status.sql
@@ -1,10 +1,5 @@
-ALTER TABLE companies ADD COLUMN IF NOT EXISTS stripe_customer_id VARCHAR(255) UNIQUE;
 
-ALTER TABLE companies ADD COLUMN IF NOT EXISTS stripe_subscription_status VARCHAR(50);
 
-UPDATE companies SET stripe_subscription_status = 'INCOMPLETE' WHERE stripe_subscription_status IS NULL;
-
-ALTER TABLE companies ALTER COLUMN stripe_subscription_status SET NOT NULL;
 
 DO $$
     BEGIN
--- src/main/resources/db/migration/V2__add_stripe_fields.sql
+++ src/main/resources/db/migration/V2__add_stripe_fields.sql
@@ -1,4 +1,6 @@
+ALTER TABLE companies ADD COLUMN IF NOT EXISTS stripe_subscription_status VARCHAR(50);
 
+ALTER TABLE companies ADD COLUMN IF NOT EXISTS stripe_customer_id VARCHAR(255) UNIQUE;
 
 UPDATE companies SET stripe_subscription_status = 'INCOMPLETE' WHERE stripe_subscription_status IS NULL;
 
