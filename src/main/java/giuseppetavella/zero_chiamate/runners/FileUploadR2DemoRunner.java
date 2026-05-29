8a9fdf9f69c4d356edf4573948c246e5ce941bba
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-05-29 08:40:53 +0200

    detect xlsx

--- src/main/java/giuseppetavella/zero_chiamate/runners/FileUploadR2DemoRunner.java
+++ src/main/java/giuseppetavella/zero_chiamate/runners/FileUploadR2DemoRunner.java
@@ -7,6 +7,7 @@ import giuseppetavella.zero_chiamate.infrastructure.email.params.EmailParams;
 import giuseppetavella.zero_chiamate.infrastructure.email_attachment.EmailAttachment;
 import giuseppetavella.zero_chiamate.infrastructure.storage.FileStorageService;
 import giuseppetavella.zero_chiamate.integrations.cloudflare_r2.CloudflareR2APIService;
+import org.apache.xmlgraphics.ps.PSImageUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -29,26 +30,30 @@ public class FileUploadR2DemoRunner implements CommandLineRunner {
     public void run(String... args) throws Exception {
         
         
-        var invoicePath = "extra/invoice.pdf";
-        var linkedinBannerPath = "extra/linkedin.png";
-        var reportTurniPath = "extra/report_turni.csv";
-        var jwtPath = "extra/report_turni.csv";
-
-        var invoiceBytes = FileHelper.readFile(invoicePath);
-        var linkedinBannerBytes = FileHelper.readFile(linkedinBannerPath);
-        var reportTurniBytes = FileHelper.readFile(reportTurniPath);
-        var jwtBytes = FileHelper.readFile(jwtPath);
-
-        var fileTypeInvoice = FileHelper.getFileType(invoiceBytes, "invoice.pdf");
-        var linkedinBannerFileType = FileHelper.getFileType(linkedinBannerBytes, "linkedin.png");
-        var reportTurniFileType = FileHelper.getFileType(reportTurniBytes, "report_turni.csv");
-        var jwtFileType = FileHelper.getFileType(jwtBytes, "jwt.txt");
+        // var invoicePath = "extra/invoice.pdf";
+        // var linkedinBannerPath = "extra/linkedin.png";
+        // var reportTurniPath = "extra/report_turni.csv";
+        // var reportTurniExcelPath = "extra/report.xlsx";
+        // var jwtPath = "extra/report_turni.csv";
         //
+        // var invoiceBytes = FileHelper.readFile(invoicePath);
+        // var linkedinBannerBytes = FileHelper.readFile(linkedinBannerPath);
+        // var reportTurniBytes = FileHelper.readFile(reportTurniPath);
+        // var reportTurniExcelBytes = FileHelper.readFile(reportTurniExcelPath);
+        // var jwtBytes = FileHelper.readFile(jwtPath);
+        //
+        // var fileTypeInvoice = FileHelper.getFileType(invoiceBytes, "invoice.pdf");
+        // var linkedinBannerFileType = FileHelper.getFileType(linkedinBannerBytes, "linkedin.png");
+        // var reportTurniFileType = FileHelper.getFileType(reportTurniBytes, "report_turni.csv");
+        // var reportTurniExcelFileType = FileHelper.getFileType(reportTurniExcelBytes, "report.xlsx");
+        // var jwtFileType = FileHelper.getFileType(jwtBytes, "jwt.txt");
+        // //
         // System.out.println(fileTypeInvoice);
         // System.out.println(linkedinBannerFileType);
         // System.out.println(reportTurniFileType);
+        // System.out.println(reportTurniExcelFileType);
         // System.out.println(jwtFileType);
-        //
+
         
         
         
