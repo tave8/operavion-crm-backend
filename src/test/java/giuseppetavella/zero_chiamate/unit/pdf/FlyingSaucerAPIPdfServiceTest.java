1a725a7274e9281eedf1e0b2d0292871b25692c5
Giuseppe Tavella <giuseppetavella8@gmail.com>
2026-06-02 04:11:13 +0200

    testing: start test Flying Saucer (pdf generation)

--- src/test/java/giuseppetavella/zero_chiamate/unit/pdf/FlyingSaucerAPIPdfServiceTest.java
+++ src/test/java/giuseppetavella/zero_chiamate/unit/pdf/FlyingSaucerAPIPdfServiceTest.java
@@ -1,14 +1,45 @@
 package giuseppetavella.zero_chiamate.unit.pdf;
 
+import giuseppetavella.zero_chiamate.infrastructure.pdf.FlyingSaucerAPIPdfService;
+import giuseppetavella.zero_chiamate.infrastructure.pdf.exceptions.FlyingSaucerAPIException;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
 class FlyingSaucerAPIPdfServiceTest {
 
+    @Autowired
+    private FlyingSaucerAPIPdfService underTest;
+    
+    
     @Test
-    void htmlToPdf() {
+    void validHtmlCanBePDF() {
+        // given
+        var html = "<p>hi</p>";
+        
+        // then
+        assertDoesNotThrow(() -> {
+            // when
+            underTest.htmlToPdf(html);
+        });
     }
+    
 
     @Test
-    void templateToPdf() {
+    void invalidHtmlCannotBePDF() {
+        // given
+        var html = "<p>hi<p>";
+
+        // then
+        assertThrows(FlyingSaucerAPIException.class, () -> {
+            // when 
+            underTest.htmlToPdf(html);
+        });
     }
+    
+
 }
